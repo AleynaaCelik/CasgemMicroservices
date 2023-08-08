9c3f8bc672dad4bf3ba8d296a6b3139d3d60f27d
aleyna çelik <celikaleyna71@gmail.com>
2023-08-08 10:16:08 +0300

    Add project files.

---
+++ Services/Catalog/CasgemMicroservices.Services.Catalog/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CasgemMicroservices.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        public string  CategoryID { get; set; }
+        public string  CategoryName { get; set; }
+    }
+}
---
+++ Services/Catalog/CasgemMicroservices.Services.Catalog/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CasgemMicroservices.Services.Catalog.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryID { get; set; }
+        public Category  Category { get; set; }
+
+
+    }
+}
