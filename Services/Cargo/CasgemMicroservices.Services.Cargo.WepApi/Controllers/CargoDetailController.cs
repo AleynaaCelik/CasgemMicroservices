68366b8edfa3d7ab48eec8bfba4e75e35b330bd5
aleyna çelik <celikaleyna71@gmail.com>
2023-08-15 09:38:05 +0300

    Add

---
+++ Services/Cargo/CasgemMicroservices.Services.Cargo.WepApi/Controllers/CargoDetailController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemMicroservices.Services.Cargo.WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoDetailController : ControllerBase
+    {
+    }
+}
